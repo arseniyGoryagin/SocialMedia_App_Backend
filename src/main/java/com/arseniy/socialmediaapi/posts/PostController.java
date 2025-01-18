3f743134fba6b6329aa1d33e62893ac683d1cb3d
Arseniy <arseniygoryagin@gmail.com>
2025-01-18 23:12:32 +0300

    added auth principal to all of these

--- src/main/java/com/arseniy/socialmediaapi/posts/PostController.java
+++ src/main/java/com/arseniy/socialmediaapi/posts/PostController.java
@@ -9,7 +9,6 @@ import com.arseniy.socialmediaapi.posts.domain.PostRequest;
 import com.arseniy.socialmediaapi.posts.domain.PostResponse;
 import com.arseniy.socialmediaapi.util.responses.MessageResponse;
 import com.arseniy.socialmediaapi.user.domain.User;
-import com.arseniy.socialmediaapi.util.Util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -17,6 +16,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -32,66 +33,46 @@ public class PostController {
 
 
     @GetMapping("/user/{username}")
-    public ResponseEntity<Page<PostResponse>> getUserPosts(@PathVariable("username") String username, @RequestParam("page") int page, @RequestParam("size") int size) {
-
+    public ResponseEntity<Page<PostResponse>> getUserPosts(@AuthenticationPrincipal UserDetails userDetails,@PathVariable("username") String username, @RequestParam("page") int page, @RequestParam("size") int size) {
         Pageable pageable = PageRequest.of(page, size);
-        return new ResponseEntity<>(postService.getAllUserPosts(username, Util.getCurrentUserUsername(), pageable), HttpStatus.OK);
+        return new ResponseEntity<>(postService.getAllUserPosts(username,userDetails.getUsername() , pageable), HttpStatus.OK);
     }
 
 
     @GetMapping()
-    public ResponseEntity<Page<PostResponse>> getFeed(@RequestParam("page") int page, @RequestParam("size") int size){
-
+    public ResponseEntity<Page<PostResponse>> getFeed(@AuthenticationPrincipal UserDetails userDetails, @RequestParam("page") int page, @RequestParam("size") int size){
         Pageable pageable = PageRequest.of(page, size);
-        return new ResponseEntity<>(postService.getPosts( Util.getCurrentUserUsername(),pageable), HttpStatus.OK);
+        return new ResponseEntity<>(postService.getPosts(userDetails.getUsername(),pageable), HttpStatus.OK);
 
     }
 
 
 
     @PostMapping()
-    public ResponseEntity<MessageResponse> addPost(@RequestBody PostRequest request) throws EmailAlreadyInUseException {
-
-        User user  = Util.getCurrentUser();
-
-        var response = postService.addPost( request.getBody(), user);
-        log.info(response.toString());
-
+    public ResponseEntity<MessageResponse> addPost(@AuthenticationPrincipal UserDetails userDetails,@RequestBody PostRequest request) throws EmailAlreadyInUseException {
+        postService.addPost( request.getBody(), (User) userDetails);
         return new ResponseEntity<>(new MessageResponse("Post added"), HttpStatus.OK);
     }
 
 
 
     @PutMapping("/{id}")
-    public ResponseEntity<MessageResponse> editPost(@PathVariable("id") Long postId, @RequestBody EditPostRequest request) throws  NotAllowedException, NoSuchException {
-
-        postService.editPost( Util.getCurrentUserUsername() ,postId, request.getNewBody());
-
+    public ResponseEntity<MessageResponse> editPost(@AuthenticationPrincipal UserDetails userDetails, @PathVariable("id") Long postId, @RequestBody EditPostRequest request) throws  NotAllowedException, NoSuchException {
+        postService.editPost( userDetails.getUsername(),postId, request.getNewBody());
         return new ResponseEntity<>(new MessageResponse("Post edited"), HttpStatus.OK);
-
-
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<MessageResponse> deletePost(@PathVariable("id") Long postId) throws NotAllowedException, NoSuchException {
-
-        User user  = Util.getCurrentUser();
-
-        postService.deletePost(user.getUsername(),  postId);
-
+    public ResponseEntity<MessageResponse> deletePost(@AuthenticationPrincipal UserDetails userDetails, @PathVariable("id") Long postId) throws NotAllowedException, NoSuchException {
+        postService.deletePost(userDetails.getUsername(),  postId);
         return new ResponseEntity<>(new MessageResponse("Post deleted"), HttpStatus.OK);
     }
 
 
     @GetMapping("/likes/{username}")
-    public ResponseEntity<Page<PostResponse>> getUserLikes(@PathVariable("username") String username, @RequestParam("page") int page, @RequestParam("size") int size){
-
-        User user = Util.getCurrentUser();
-
+    public ResponseEntity<Page<PostResponse>> getUserLikes(@AuthenticationPrincipal UserDetails userDetails, @PathVariable("username") String username, @RequestParam("page") int page, @RequestParam("size") int size){
         Pageable pageable = PageRequest.of(page, size);
-
-        Page<PostResponse> likes = postService.getUserLikes(user.getUsername(),pageable);
-
+        Page<PostResponse> likes = postService.getUserLikes(userDetails.getUsername(),pageable);
         return new ResponseEntity<>(likes, HttpStatus.OK);
     }
 
