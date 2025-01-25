17a8d2e7863cb78e7188988e3f3d8243d3cdc9c9
Arseniy <arseniygoryagin@gmail.com>
2025-01-25 14:05:23 +0300

    updated th jwt servic

---
+++ src/main/java/com/arseniy/socialmediaapi/auth/config/AuthConfig.java
@@ -0,0 +1,14 @@
+package com.arseniy.socialmediaapi.auth.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties
+@Data
+public class AuthConfig {
+
+
+}
